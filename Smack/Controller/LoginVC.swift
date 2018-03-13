c7abe69064fb866784fb3dd1a28886d1d8cb23f1
Steve Drzewiczewski <steve@stevedandrebecca.com>
2018-03-12 20:01:50 -0400

    lesson-04

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Drzewiczewski, Steve on 3/12/18.
+//  Copyright © 2018 Drzewiczewski, Steve. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closedPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Drzewiczewski, Steve on 3/12/18.
+//  Copyright © 2018 Drzewiczewski, Steve. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"

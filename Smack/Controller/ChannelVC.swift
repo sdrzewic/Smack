fcd184a544d9b7dc7259294f5b33510c28284dc9
Steve Drzewiczewski <steve@stevedandrebecca.com>
2018-03-19 23:53:55 -0400

    lesson-13

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ChannelVC: UIViewController {
     //Outlets
+    @IBOutlet weak var userImage: CircleImage!
     @IBOutlet weak var loginBtn: UIButton!
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue){}
     
@@ -17,11 +18,23 @@ class ChannelVC: UIViewController {
         super.viewDidLoad()
 
         self.revealViewController().rearViewRevealWidth = self.view.frame.width - 60
+        NotificationCenter.default.addObserver(self, selector: #selector (ChannelVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+        
     }
-
 
     @IBAction func loginBtnPressed(_ sender: Any) {
         performSegue(withIdentifier: TO_LOGIN, sender: nil)
     }
     
+    @objc func userDataDidChange(_ notif: Notification){
+        if AuthService.instance.isLoggedIn {
+            loginBtn.setTitle(UserDataService.instance.name, for: .normal)
+            userImage.image = UIImage(named: UserDataService.instance.avatarName)
+            userImage.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+        } else {
+            loginBtn.setTitle("Login", for: .normal)
+            userImage.image = UIImage(named: "menuProfileIcon")
+            userImage.backgroundColor = UIColor.clear
+        }
+    }
 }
